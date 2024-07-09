4f79825b4775e3aad197f630c5c18e8d9bfa2bdc
casper <oksanaozturk@gmail.com>
2024-07-09 18:03:07 +0300

    Настройка отображения сущностей модели  User для Админки

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
-# Register your models here.
+from users.models import User
+
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    """Класс для регистрации User в админке."""
+
+    list_display = ("id", "email", "is_active", "password", "first_name", "second_name")
+    list_filter = ("email",)
+    search_fields = ("email",)
