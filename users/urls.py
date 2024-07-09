689717a61ae8e5b283e4abac9f291fa6f7bd03b9
casper <oksanaozturk@gmail.com>
2024-07-09 18:02:19 +0300

    Создание путей для CRUD модели User через создание router

--- users/urls.py
+++ users/urls.py
@@ -1,7 +1,14 @@
+from rest_framework.routers import SimpleRouter
+
 from users.apps import UsersConfig
+from users.views import UserViewSet
 
 app_name = UsersConfig.name
 
+# Создаем экземпляр класса SimpleRouter(), он обеспечивает маршрутизацию всех путей CRUD
+router = SimpleRouter()
+router.register("users", UserViewSet)
+
 urlpatterns = [
 
-    ]
+    ] + router.urls
