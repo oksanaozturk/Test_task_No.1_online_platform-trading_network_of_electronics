d9e1f58eacbff60238c1dedf52f83bc095326c8a
casper <oksanaozturk@gmail.com>
2024-07-09 18:00:27 +0300

    Создание CRUD для модели User на основе ViewSet

--- users/views.py
+++ users/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet
 
-# Create your views here.
+from users.models import User
+from users.serializers import UserSerializer
+
+
+class UserViewSet(ModelViewSet):
+    """Класс для настройки CRUD для модели User с помощью метода ViewSet
+    Create, Update, Retrieve, Delete."""
+    serializer_class = UserSerializer
+    # Получаем все данне из БД
+    queryset = User.objects.all()
