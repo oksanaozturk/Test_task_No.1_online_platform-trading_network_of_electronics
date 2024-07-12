612136ee3ba350ebc0546b4ce124a09b915d1a2f
casper <oksanaozturk@gmail.com>
2024-07-12 23:40:50 +0300

    Добавлены контроллеры для сущностей моделей Product и NetworkObject

--- network/views.py
+++ network/views.py
@@ -1,3 +1,34 @@
-from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet
 
-# Create your views here.
+from network.models import Product, NetworkObject
+from network.serializers import ProductSerializer, NetworkObjectSerializer, NetworkObjectDetailSerializer
+
+
+class ProductViewSet(ModelViewSet):
+    """
+    Класс настройки CRUD для модели Product с помощью метода ViewSet
+    Create, Update, Retrieve, Delete.
+    """
+    serializer_class = ProductSerializer
+    # Получаем все данне из БД
+    queryset = Product.objects.all()
+
+
+class NetworkObjectViewSet(ModelViewSet):
+    """
+    Класс настройки CRUD для модели NetworkObject с помощью метода ViewSet
+    Create, Update, Retrieve, Delete.
+    """
+    serializer_class = NetworkObjectSerializer
+    queryset = NetworkObject.objects.all()
+
+    def get_serializer_class(self):
+        """
+        Метод получения сериализатора в зависимости от запроса
+        (вывод всего списка или просмотр одного объекта).
+        """
+
+        if self.action == "retrieve":
+            return NetworkObjectDetailSerializer
+
+        return NetworkObjectSerializer
