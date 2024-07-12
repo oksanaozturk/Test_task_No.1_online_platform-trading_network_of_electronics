335acdafc83bc2ff7f62c345454fe5ce1e8a921a
casper <oksanaozturk@gmail.com>
2024-07-12 22:32:43 +0300

    Добавлено приложение network

--- config/settings.py
+++ config/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     "users",
+    "network",
     "drf_yasg",
     "rest_framework",
     "rest_framework_simplejwt",
