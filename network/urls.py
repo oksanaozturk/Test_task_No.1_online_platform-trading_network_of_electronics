ee6f9c8e63991cd3876786bd709818f1e01400e8
casper <oksanaozturk@gmail.com>
2024-07-12 23:40:28 +0300

    Добавленs пути через создание роутера для сущностей моделей Product и NetworkObject

---
+++ network/urls.py
@@ -0,0 +1,16 @@
+from rest_framework.routers import SimpleRouter
+
+from network.apps import NetworkConfig
+from network.views import ProductViewSet, NetworkObjectViewSet
+
+app_name = NetworkConfig.name
+
+# Создаем экземпляр класса SimpleRouter(), он обеспечивает маршрутизацию всех путей CRUD
+# [HttpGet]
+router = SimpleRouter()
+router.register("products", ProductViewSet)
+router.register("networkobjects", NetworkObjectViewSet)
+
+urlpatterns = [
+
+    ] + router.urls
