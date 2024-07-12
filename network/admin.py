656e134984cdb1c953bfa6b2f9b87c2e922b1837
casper <oksanaozturk@gmail.com>
2024-07-12 23:41:13 +0300

    Добавлено отображение в Админке для сущностей моделей Product и NetworkObject

--- network/admin.py
+++ network/admin.py
@@ -1,3 +1,25 @@
 from django.contrib import admin
 
-# Register your models here.
+from network.models import Product, NetworkObject
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    """
+    Класс для регистрации  модели Product в админке.
+    """
+
+    list_display = ("id", "name", "model", "launch_date")
+    list_filter = ("name",)
+    search_fields = ("name",)
+
+
+@admin.register(NetworkObject)
+class NetworkObjectAdmin(admin.ModelAdmin):
+    """
+    Класс для регистрации модели NetworkObject в админке.
+    """
+
+    list_display = ("id", "name", "email", "country", "town", "provider", "debt_to_provider", "time_of_creation")
+    list_filter = ("name",)
+    search_fields = ("name", "country")
