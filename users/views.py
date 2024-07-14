858fef59785614cc6a272b5d57b3aae4b515093f
casper <oksanaozturk@gmail.com>
2024-07-14 03:53:24 +0300

    Добавлен метод для кеширования пароля

--- users/views.py
+++ users/views.py
@@ -11,3 +11,14 @@ class UserViewSet(ModelViewSet):
     serializer_class = UserSerializer
     # Получаем все данне из БД
     queryset = User.objects.all()
+
+    def perform_create(self, serializer):
+        """
+        Данный метод необходим для того, чтобы нам не мешала настройка 'username = None',
+        указанная нами в модели User в models.py
+        """
+
+        user = serializer.save(is_active=True)
+        # Кешируем обязательно пароль Пользователя
+        user.set_password(user.password)
+        user.save()
