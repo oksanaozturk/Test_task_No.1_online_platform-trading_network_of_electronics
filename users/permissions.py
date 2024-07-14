a8bc7e1aa09d9b9cb6782f48f41d1b8864057892
casper <oksanaozturk@gmail.com>
2024-07-14 22:39:01 +0300

    Удален лишний код

--- users/permissions.py
+++ users/permissions.py
@@ -13,20 +13,3 @@ class IsActiveAndIsStaff(permissions.BasePermission):
         Метод проверки на то, что пользователь is_active и is_staff.
         """
         return request.user.is_active and request.user.is_staff
-
-    # def has_object_permission(self, request, view, obj):
-    #     """
-    #     Метод Запрета на редактирование поля debt_to_provider.
-    #     """
-
-        # if obj.debt_to_provider != request.obj.debt_to_provider:
-        #     print("Поле debt_to_provider не подлежит изменению")
-        #     return False
-
-        # Read permissions are allowed to any request,
-        # so we'll always allow GET, HEAD or OPTIONS requests.
-        # if obj.debt_to_provider and request.method in ["PUT", "PATCH"]:
-        #     return False
-
-        # Write permissions are only allowed to the owner of the snippet.
-        # return obj.debt_to_provider
